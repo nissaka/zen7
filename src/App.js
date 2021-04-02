c305dc542989bf33e856d6f5f7e0f4f68f947d72
huangw <maigechuang115@163.com>
2021-04-02 11:16:54 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      innerWidth: 0,
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+  handleResize = (e) => {
+    this.setState({ innerWidth: e.target.innerWidth });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {console.log(
+            this.state.innerWidth < 1080,
+            typeof this.state.innerWidth
+          )}
+          {this.state.innerWidth > 740 ? (
+            <img
+              src="./img/zen7_desktop_dark.png"
+              className="desktop"
+              alt="building"
+            />
+          ) : (
+            <img
+              src="./img/zen7_mobile_dark.png"
+              className="mobile"
+              alt="building"
+            />
+          )}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
